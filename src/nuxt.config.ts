1c75bab9f4a9f1d879315a68ccac12efeab66620
Danila Kiva <kivadan@inbox.ru>
2024-01-21 13:34:06 +0300

    fix dark mode

--- src/nuxt.config.ts
+++ src/nuxt.config.ts
@@ -1,10 +1,13 @@
+// import { theme } from './.nuxt/types/tailwind.config';
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@nuxt/ui'],
   devtools: { enabled: true },
+  colorMode: {
+    preference: 'light'
+  },
   typescript: {
     strict: true,
     typeCheck: false,
-  },
-
+  }
 })
